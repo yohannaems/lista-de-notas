ecf10d559a3b4f63a0a284ec848cc8c620e40efc
PMENOS.COM.BR\116132 <yohannabarbosa@pmenos.com.br>
2022-01-14 16:40:14 -0300

    atualizando projeto

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./estilo.css"
+import deleteSVG from "../../assets/img/delete.svg"
 class CardNota extends React.Component {
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <img src={deleteSVG}/>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
