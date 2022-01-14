7693694f69ef4ba2ad5760df1fc72290baa1f034
PMENOS.COM.BR\116132 <yohannabarbosa@pmenos.com.br>
2022-01-14 09:03:55 -0300

    Salvando meu primeiro projeto com react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeNotas from "./components/ListaDeNotas";
+import "./assets/App.css";
+import "./assets/index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();    
+    this.state = {
+      notas: []
+    }
+  }
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novaArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas: novaArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
+      </section>
+    );
+  }
 }
 
 export default App;
